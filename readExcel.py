afb4adcd8c86e107955991bf196f7f0b09c689b3
GWANSU SHIN <76953652+happysun7080@users.noreply.github.com>
2021-05-14 06:39:04 +0900

    2021-05-13 말뭉치 데이터 읽어오기
    pandas, openpyxl, xlrd module로 확장자가 xlsx인 말뭉치 데이터를 읽어오기
    xlrd, pandas, openpyxl 모듈 설치하고 진행

--- readExcel.py
+++ readExcel.py
@@ -9,7 +9,7 @@ excelTitle = "" + "1_구어체(1)_200226"  # 경로 + 파일명
 excel_dafaframe = pd.read_excel(excelTitle + ".xlsx", engine='openpyxl')  # 원본 excel 파일
 original_eng_list = list((excel_dafaframe['번역문']))  # 영어 문장 (번역문)
 original_kor_list = list((excel_dafaframe['원문']))  # 한국어 문장 (원문)
-
+ 
 # example
 print(original_eng_list[0])
 print(original_kor_list[0])
