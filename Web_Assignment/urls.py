8c8a747a44e64b3e990e41f4433da9e73ee87acf
WoosungMichael <woosung0420k@naver.com>
2021-06-11 23:40:31 +0900

    2021-06-11 이미지 첨부 위한 코드 추가

--- Web_Assignment/urls.py
+++ Web_Assignment/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('trans/', views.trans, name='trans'),
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
